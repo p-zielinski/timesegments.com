c02b90c6e15849aa782aa145ebb094ee3a5fd205
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-01-08 22:07:15 +0100

    in progress

--- apps/frontend/pages/dashboard/categories.tsx
+++ apps/frontend/pages/dashboard/categories.tsx
@@ -14,6 +14,7 @@ import DashboardLayout from '../../layouts/dashboard';
 import { ColumnSortOptions } from '../../enums/sortOption';
 import { sortCategories } from '../../utils/sortCategories';
 import { UserWithCategoriesAndSubcategories } from '../../type/user';
+import {shared} from "../../../../libs/shared/src";
 
 // ----------------------------------------------------------------------
 
