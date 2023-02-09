0544fc1ac7973e79a9ec3733ca775fa8a715af2b
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-02-09 22:01:53 +0100

    Update next.config.js

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,6 +7,13 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
