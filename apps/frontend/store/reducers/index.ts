58c38fd5d8e2ff9460adf0459f10484e1257e694
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-02-19 14:02:40 +0100

    in progress

--- apps/frontend/store/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import categoriesReducer from './categoriesReducer';
-
-export default combineReducers({
-  categories: categoriesReducer,
-});
---
+++ prisma/migrations/20230219110554_schema_v1_3_4/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "controlValue" TEXT NOT NULL DEFAULT 'new user';
