415382957bc95911785cf4daa217bd59fc91c821
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-04-23 23:00:31 +0200

    Update ecosystem.config.example.js

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'timesegs-frontend',
       version: '1.0.0',
-      script: 'npm run start:frontend',
+      script: 'npm run start:frontend -- port=20122',
       env: {
         NEXT_PUBLIC_APP_NAME: 'Timesegs',
         NEXT_PUBLIC_API_URL: 'https://timesegs-backend.cytr.us/api/',
@@ -12,7 +12,7 @@ module.exports = {
     {
       name: 'timesegs-backend',
       version: '1.0.0',
-      script: 'npm run start:backend',
+      script: 'npm run start:backend -- port=30122',
       env: {
         DATABASE_URL:
           'postgres://postgres:ABkNPAC6fs@mws02.mikr.us:50002/timesegs_prod',
