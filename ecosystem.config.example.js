77401e6ba955d8400905c61d59e10faa8e6d4351
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-04-23 22:45:32 +0200

    try

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -3,16 +3,16 @@ module.exports = {
     {
       name: 'timesegs-frontend',
       version: '1.0.0',
-      script: 'nx serve frontend --prod --port=20122 --hostname 0.0.0.0',
+      script: 'npm run start:frontend',
       env: {
         NEXT_PUBLIC_APP_NAME: 'Timesegs',
         NEXT_PUBLIC_API_URL: 'https://timesegs-backend.cytr.us/api/',
       },
     },
     {
-      name: 'timesegs-frontend',
+      name: 'timesegs-backend',
       version: '1.0.0',
-      script: 'prisma migrate dev; nx serve backend --prod --port=$PORT',
+      script: 'npm run start:backend',
       env: {
         DATABASE_URL:
           'postgres://postgres:ABkNPAC6fs@mws02.mikr.us:50002/timesegs_prod',
